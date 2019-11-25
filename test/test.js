98e737a562bff28bc22e3f228017c43e88662118
Hyunwoo Lee <aeharvlee@gmail.com>
2019-11-25 23:09:37 +0900

    Update: refactor test code

--- test/test.js
+++ test/test.js
@@ -5,18 +5,27 @@ const Recall = artifacts.require("Recall")
 contract("Modoo's Recall Test", async accounts => {
   let koreaVehicleVendors = undefined
   let recall = undefined
-  before("setup contract", async accounts => {
+  let owner = undefined
+
+  before("setup contract", async () => {
+    console.log("setup contract")
     koreaVehicleVendors = await KoreaVehicleVendors.deployed()
     recall = await Recall.deployed()
   })
 
-  it("owner must be accounts[0]", async accounts => {
-    const owner = await koreaVehicleVendors.owner.call()
-    assert(owner, accounts[0])
-  }) 
+  contract("Contract KoreaVehicleVendors Test", async () => {
+    it("owner must be accounts[0]", async () => {
+      console.log("owner must be accounts[0]")
+      owner = await koreaVehicleVendors.owner.call()
+      assert(owner, accounts[0])
+    }) 
+  })
 
-  it("owner must be accounts[0]", async accounts => {
-    const owner = await recall.owner.call()
-    assert(owner, accounts[0])
+  contract("Contract Recall Test", async () => {
+    it("owner must be accounts[0]", async () => {
+      console.log("owner must be accounts[0]")
+      owner = await recall.owner.call()
+      assert(owner, accounts[0])
+    })
   })
 })

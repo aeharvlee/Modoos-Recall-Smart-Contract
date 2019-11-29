218514f99c7a121488c9bd1a6578fe7d18b546b8
Hyunwoo Lee <aeharvlee@gmail.com>
2019-11-30 00:51:35 +0900

    Add: action which creates recall state

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ const morgan = require('morgan')
 const Caver = require('caver-js')
 const caver = new Caver('https://api.baobab.klaytn.net:8651')
 const contract = require(`${__dirname}/../config/contract`)
+const keystore = require(`${__dirname}/../config/keystore`)
 
 const koreaVehicleVendors = contract.koreaVehicleVendors
 const recall = contract.recall
+const keyChain = keystore.keyChain
 
 const app = express()
 
@@ -22,7 +24,33 @@ const result = [
 ]
 
 app.get('/', async (req, res) => {
-  res.send(result)
+  const feePayer = await caver.klay.accounts.wallet.add(
+    keyChain['government']['privateKey'],
+    keyChain['government']['address'],
+  )
+
+  const data = req.body
+  const abiCreateRecall = recall.methods.createRecall(
+    helper.stringToBytes32(data.primaryKey),
+    keyChain[data.vendorName]['address'], 
+    helper.stringToBytes32(data.vehicleRegistrationNumber)
+  ).encodeABI()
+
+  let receipt = undefined
+  try {
+    receipt = await helper.feeDelegatedSmartContractExecute(
+      keyChain[data.vendorName]['address'],
+      keyChain[data.vendorName]['privateKey'],
+      recall._address,
+      feePayer,
+      abiCreateRecall
+    )
+  } catch (error) {
+    console.log(error)
+  }
+
+  console.log(receipt)
+
 })
 
 app.listen(3001, () => {

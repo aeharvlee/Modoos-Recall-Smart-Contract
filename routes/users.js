40c455b64475a435b40dc93a87481393b6ac7b29
Hyunwoo Lee <aeharvlee@gmail.com>
2019-11-30 05:56:46 +0900

    Add: proceed recall function

--- routes/users.js
+++ routes/users.js
@@ -25,13 +25,14 @@ kia = caver.klay.accounts.wallet.add(
   keyChain['kia']['address']
 )
 
+// 진정 생성된 것인지 다시한 번 확인 필요
 router.get('/create', async function(req, res, next) {
+  // get request params
 	docno = req.query.infoId
 	address = req.query.user_id
 	user_privatekey = req.query.user_privatekey
 	vendorName = req.query.vendor_name
   registrationNumber = req.query.serialno
-
 
 	const abiCreateRecall = recall.methods.createRecall(
 		helper.stringToBytes32(docno),
@@ -72,4 +73,45 @@ router.get('/create', async function(req, res, next) {
   res.send(receipt);
 });
 
+router.get('/proceed', async function(req, res, next) {
+  // get request params
+  docno = req.query.infoId
+  vendorName = req.query.vendor_name
+
+	const abiProceedRecall = recall.methods.proceedRecall(
+		helper.stringToBytes32(docno),
+  ).encodeABI()
+
+  let feeDelegatedSmartContractObject = {
+    type: 'FEE_DELEGATED_SMART_CONTRACT_EXECUTION',
+    from: keyChain[vendorName]['address'],
+    to: recall._address,
+    data: abiProceedRecall,
+    gas: '300000',
+  };
+
+  let rlpEncodedTransaction = null;
+  try {
+      rlpEncodedTransaction = await caver.klay.accounts.signTransaction(
+          feeDelegatedSmartContractObject,
+		      keyChain[vendorName]['privateKey'], 
+      );
+  } catch (error) {
+      console.log(error);
+      throw Error(error);
+  }
+
+  let receipt = null;
+  try {
+      receipt = await caver.klay.sendTransaction({
+          senderRawTransaction: rlpEncodedTransaction.rawTransaction,
+          feePayer: feePayer.address
+      });
+  } catch (error) {
+      throw Error(error);
+  }
+
+  console.log(receipt)
+  res.send(receipt);
+});
 module.exports = router;
